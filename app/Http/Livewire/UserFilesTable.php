f429ab4416c72da9c08ea8f6bd9dd1d74a065d1f
xavier <xavierserna472@gmail.com>
2024-01-30 14:43:36 -0500

    update

--- app/Http/Livewire/UserFilesTable.php
+++ app/Http/Livewire/UserFilesTable.php
@@ -40,27 +40,6 @@ class UserFilesTable extends Component implements Tables\Contracts\HasTable
             TextColumn::make('updated_at')
             ->sortable()
             ->searchable(),
-/* 
-            //Tables\Columns\TextColumn::make('id')->label('ID'),
-            //Tables\Columns\TextColumn::make('user_id')->label('User ID'),
-            Tables\Columns\TextColumn::make('file_name')->label('File Name')->searchable()->sortable(),
-
-            //Tables\Columns\TextColumn::make('file_path')->label('File Path'),
-            Tables\Columns\TextColumn::make('size')->label('Size')->searchable()->sortable(),
-            Tables\Columns\TextColumn::make('type')->label('Type'),
-            Tables\Columns\TextColumn::make('file_count')->label('file_count'),
-            //Tables\Columns\BooleanColumn::make('is_public')->label('Is Public'),
-            //Tables\Columns\TextColumn::make('allowed_user_ids') ->label('Allowed User IDs'),
-            //Tables\Columns\TextColumn::make('categories') ->label('Categories'),
-            //Tables\Columns\TextColumn::make('upload') ->label('Upload'),
-            //Tables\Columns\TextColumn::make('description')->label('Description'),
-            //Tables\Columns\TextColumn::make('public_key')->label('Public Key'),
-            //Tables\Columns\TextColumn::make('tags') ->label('Tags'),
-            //Tables\Columns\BooleanColumn::make('fav')->label('Favorite'),
-
-            Tables\Columns\TextColumn::make('created_at') ->label('Upload Date'),
-            Tables\Columns\TextColumn::make('updated_at') ->label('Update Date'),
-             */
         ];
     }
 
